71e0e7b339966c865b1aefa5a3c9153b67d945dc
Tacho2 <egbertcm23@gmail.com>
2024-07-22 14:29:50 -0400

    Print to file and Gonality 2

--- gonality.py
+++ gonality.py
@@ -3,7 +3,11 @@ from itertools import combinations
 from mpl_toolkits import mplot3d
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
+orig_stdout = sys.stdout
+f = open('out.txt', 'w')
+sys.stdout = f
 #MIN from r to rn of k plus the sum from 1 to r-1 of (i)#((k/r-i+1) < l < (k/r-i)) plus r#(l>k)
 
 def generateGraphs(maxWeights):
@@ -14,6 +18,7 @@ def generateGraphs(maxWeights):
                 for d in range(1, maxWeights):
                     for e in range(1, maxWeights):
                         for f in range(1, maxWeights):
+                            #for g in range(1, maxWeights):
                             Graphs.append([a, b, c, d, e, f])
 
     return Graphs
@@ -39,6 +44,19 @@ def computeGonalities(Graph, maxComputedGonality):
                 winningDivisorChipCount += (i) * count
             countArr.append(tail)
             
+            if r == 2:
+                maxLower = 0
+                minUpper = float('inf')
+                for l in Graph:
+                    if l <= maxMiddleChips/2:
+                        if l > maxLower:
+                            maxLower = l
+                    if l > maxMiddleChips/2:
+                        if l < minUpper: 
+                            minUpper = l
+                if minUpper + maxLower <= maxMiddleChips:
+                    winningDivisorChipCount -= 1
+                
             #print("Current Winning Divisor Chip Count: " +str(winningDivisorChipCount) + " Chips in middle: " +str(maxMiddleChips))
             if winningDivisorChipCount < minWinningDivisorChipCount:
                 minWinningDivisorChipCount = winningDivisorChipCount
@@ -47,31 +65,40 @@ def computeGonalities(Graph, maxComputedGonality):
         GonalitySequence.append(minWinningDivisorChipCount)
         winningConfigurations.append(winningConfig)
     
-    return winningConfigurations, GonalitySequence
+    return Graph, winningConfigurations, GonalitySequence
 
 def plotGraphs():
-    Graphs = generateGraphs(8)
+    Graphs = generateGraphs(7)
     Gonalities = [[], [], []]
+    interestingGraphs = []
     for G in Graphs:
-        currnetGons = computeGonalities(G, 3)
+        graph, winningConfigs, currnetGons = computeGonalities(G, 3)
         Gonalities[0].append(currnetGons[0])
         Gonalities[1].append(currnetGons[1])
         Gonalities[2].append(currnetGons[2])
-        print("Graph: " +str(G)+ ", Gonalities:" + str(currnetGons))
+        if (currnetGons[1] < (3/2) * currnetGons[0]):
+            interestingGraphs.append(graph)
+            print("Graph: " +str(graph)+ ", Gonalities:" + str(currnetGons))
+        #print("Graph: " +str(G)+ ", Gonalities:" + str(currnetGons))
 
     # Create a 3D plot
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.set_xlabel('Gon-2')
-    ax.set_ylabel('Gon-3')
+    ax.set_xlabel('Gon-1')
+    ax.set_ylabel('Gon-2')
     #ax.set_zlabel('Gon-3')
 
-    ax.scatter(Gonalities[1], Gonalities[2], c='k', marker='x', label='None')
+    ax.scatter(Gonalities[0], Gonalities[1], c='k', marker='x', label='None')
 
     plt.show()
     
-    
+   
 Graph = [9, 9, 9, 9, 9, 9, 5, 2, 2]
-configs, gonSeq = computeGonalities(Graph, 3)
+graph, configs, gonSeq = computeGonalities(Graph, 3)
 splitGraph = [[] for _ in range(len(configs))]
 print("Graph + configs: " +str(configs)+ " Gonalities: " + str(gonSeq))
+
+plotGraphs()
+
+sys.stdout = orig_stdout
+f.close()
